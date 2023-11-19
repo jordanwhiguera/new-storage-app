fb90b36f4475fc91229f146347e1114ca19bae88
Jordan Higuera <jordanwhiguera@gmail.com>
2023-11-18 21:31:27 -0500

    added register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,10 +4,45 @@ import axios from "axios";
 import { FaGoogle } from "react-icons/fa";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
-  return <div>RegisterModal</div>;
+  const [isLoading, setLoading] = React.useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { name: "", email: "", password: "" } });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+    axios
+      .post("/api/register", data)
+      .then((res) => {
+        registerModal.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  // Add your form JSX here
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.close}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
 };
 
 export default RegisterModal;
