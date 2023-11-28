fc2bcdab2b024705823e74d3d2e31d0fa6333242
Jordan Higuera <jordanwhiguera@gmail.com>
2023-11-27 20:48:47 -0500

    blocked dates from calandar once reserved

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { categories } from "@/app/components/navbar/Categories";
-import { SafeListing, SafeUser } from "@/app/types";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 import Container from "@/app/components/Container";
 import React, { use } from "react";
@@ -26,7 +26,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-  reservations?: Reservation[];
+  reservations?: SafeReservation[];
   listing: SafeListing & {
     user: SafeUser;
   };
