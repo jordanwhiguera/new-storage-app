72f0709c2647517b8a110b87efc6e3c92406000b
Jordan Higuera <jordanwhiguera@gmail.com>
2023-11-29 20:59:01 -0500

    fixed menu not displaying over items

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -57,7 +57,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
-      className="col-span-1 cursor-pointer group"
+      className="col-span-1 cursor-pointer group -z-10"
     >
       <div className="flex flex-col gap-2 w-full">
         <div className="aspect-square w-full relative overflow-hidden rounded-xl">
