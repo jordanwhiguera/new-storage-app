14156b73b0ea8055e8ecff9404254241c61aa2d6
Jordan Higuera <jordanwhiguera@gmail.com>
2023-12-05 15:33:15 -0500

    added coordinate functionaly to useLocation

--- app/components/inputs/LocationSelect.tsx
+++ app/components/inputs/LocationSelect.tsx
@@ -30,13 +30,62 @@
 
 // export default LocationSelect;
 
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import GooglePlacesAutocomplete, {
+//   geocodeByPlaceId,
+// } from "react-google-places-autocomplete";
+// import useLocation from "@/app/hooks/useLocation"; // Adjust the path as needed
+// import { LocationValue } from "@/app/hooks/useLocation"; // Adjust the path as needed
+
+// interface LocationSelectProps {
+//   value?: LocationValue;
+//   onChange: (value: LocationValue) => void;
+// }
+
+// const LocationSelect: React.FC<LocationSelectProps> = ({ value, onChange }) => {
+//   const { selectedLocation, handleChange } = useLocation(onChange, value);
+//   const [coordinates, setCoordinates] = useState<{
+//     lat: number | null;
+//     lng: number | null;
+//   }>({ lat: null, lng: null });
+
+//   useEffect(() => {
+//     if (selectedLocation?.value.place_id) {
+//       geocodeByPlaceId(selectedLocation.value.place_id)
+//         .then((results) => {
+//           const { lat, lng } = results[0].geometry.location;
+//           setCoordinates({ lat: lat(), lng: lng() });
+//         })
+//         .catch((error) => console.error("Error", error));
+//     }
+//   }, [selectedLocation]);
+
+//   useEffect(() => {
+//     console.log(coordinates); // Log the latitude and longitude
+//   }, [coordinates]);
+
+//   return (
+//     <div>
+//       <GooglePlacesAutocomplete
+//         apiKey={process.env.NEXT_PUBLIC_API_KEY}
+//         selectProps={{
+//           value: selectedLocation,
+//           onChange: handleChange,
+//         }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default LocationSelect;
+
 "use client";
-import React, { useEffect, useState } from "react";
-import GooglePlacesAutocomplete, {
-  geocodeByPlaceId,
-} from "react-google-places-autocomplete";
-import useLocation from "@/app/hooks/useLocation"; // Adjust the path as needed
-import { LocationValue } from "@/app/hooks/useLocation"; // Adjust the path as needed
+import React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import useLocation from "@/app/hooks/useLocation";
+import { LocationValue } from "@/app/hooks/useLocation";
+// Adjust the path as needed
 
 interface LocationSelectProps {
   value?: LocationValue;
@@ -44,25 +93,18 @@ interface LocationSelectProps {
 }
 
 const LocationSelect: React.FC<LocationSelectProps> = ({ value, onChange }) => {
-  const { selectedLocation, handleChange } = useLocation(onChange, value);
-  const [coordinates, setCoordinates] = useState<{
-    lat: number | null;
-    lng: number | null;
-  }>({ lat: null, lng: null });
+  // useLocation now also provides the coordinates
+  const { selectedLocation, handleChange, coordinates } = useLocation(
+    onChange,
+    value
+  );
 
-  useEffect(() => {
-    if (selectedLocation?.value.place_id) {
-      geocodeByPlaceId(selectedLocation.value.place_id)
-        .then((results) => {
-          const { lat, lng } = results[0].geometry.location;
-          setCoordinates({ lat: lat(), lng: lng() });
-        })
-        .catch((error) => console.error("Error", error));
+  // You can use useEffect here if you want to perform any action when coordinates change
+  React.useEffect(() => {
+    if (coordinates.lat && coordinates.lng) {
+      console.log("Coordinates: ", coordinates);
+      // Perform any action you need with the new coordinates
     }
-  }, [selectedLocation]);
-
-  useEffect(() => {
-    console.log(coordinates); // Log the latitude and longitude
   }, [coordinates]);
 
   return (
--- app/hooks/useLocation.tsx
+++ app/hooks/useLocation.tsx
@@ -1,6 +1,42 @@
-// useLocation.tsx
+// // useLocation.tsx
+// import React, { useState, useEffect } from "react";
+// import { ActionMeta, SingleValue } from "react-select";
+
+// export interface LocationValue {
+//   label: string;
+//   value: any; // Adjust based on actual data structure
+// }
+
+// const useLocation = (
+//   onChange: (value: LocationValue) => void,
+//   initialValue?: LocationValue
+// ) => {
+//   const [selectedLocation, setSelectedLocation] = useState<
+//     LocationValue | undefined
+//   >(initialValue);
+
+//   useEffect(() => {
+//     setSelectedLocation(initialValue);
+//   }, [initialValue]);
+
+//   const handleChange = (
+//     newValue: SingleValue<LocationValue>,
+//     actionMeta: ActionMeta<LocationValue>
+//   ) => {
+//     if (actionMeta.action === "select-option" && newValue) {
+//       setSelectedLocation(newValue);
+//       onChange(newValue);
+//     }
+//   };
+
+//   return { selectedLocation, handleChange };
+// };
+
+// export default useLocation;
+
 import React, { useState, useEffect } from "react";
 import { ActionMeta, SingleValue } from "react-select";
+import { geocodeByPlaceId } from "react-google-places-autocomplete";
 
 export interface LocationValue {
   label: string;
@@ -14,10 +50,27 @@ const useLocation = (
   const [selectedLocation, setSelectedLocation] = useState<
     LocationValue | undefined
   >(initialValue);
+  const [coordinates, setCoordinates] = useState<{
+    lat: number | null;
+    lng: number | null;
+  }>({ lat: null, lng: null });
 
   useEffect(() => {
     setSelectedLocation(initialValue);
+    // Reset coordinates when the initial value changes
+    setCoordinates({ lat: null, lng: null });
   }, [initialValue]);
+
+  useEffect(() => {
+    if (selectedLocation?.value.place_id) {
+      geocodeByPlaceId(selectedLocation.value.place_id)
+        .then((results) => {
+          const { lat, lng } = results[0].geometry.location;
+          setCoordinates({ lat: lat(), lng: lng() });
+        })
+        .catch((error) => console.error("Error fetching coordinates:", error));
+    }
+  }, [selectedLocation]);
 
   const handleChange = (
     newValue: SingleValue<LocationValue>,
@@ -29,7 +82,7 @@ const useLocation = (
     }
   };
 
-  return { selectedLocation, handleChange };
+  return { selectedLocation, handleChange, coordinates };
 };
 
 export default useLocation;
