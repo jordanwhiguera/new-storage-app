8d1c7164a8ebc8023189f92fdd93b6fa764997e9
Jordan Higuera <jordanwhiguera@gmail.com>
2023-12-14 14:39:01 -0500

    Update ListingReservation component styling

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -58,7 +58,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
           isSingleDateSelection={isMonthToMonth}
         />
         {isMonthToMonth && (
-          <div className="absolute top-4 right-4 w-52 md:w-28 lg:w-36 lg:right-10 h-9 bg-white flex justify-center items-center">
+          <div className="absolute top-4 right-4 w-52 md:w-28 lg:w-36 lg:right-10 h-9 bg-white flex justify-center items-center custom-right-md custom-right-md-two custom-right-lg">
             <FaInfinity size={32} />
           </div>
         )}
