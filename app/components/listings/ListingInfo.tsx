d05d8372cf4f555bdf5fde6ab2bf8f4b205286b4
Jordan Higuera <jordanwhiguera@gmail.com>
2023-12-16 17:55:05 -0500

    contact host button styling

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -37,13 +37,11 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   return (
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2 mb-2">
           <div>Hosted by {firstName}</div>
           <Avatar src={user?.image} />
         </div>
-        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
-          <Button smallWidth label="Contact host" onClick={() => {}} />
-        </div>
+        <Button smallWidth label="Contact host" onClick={() => {}} />
       </div>
       <hr />
       {category && (
